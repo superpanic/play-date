31759328a489d70649b6a6af93219441fa9e27c2
Fredrik Josefsson <fredrik@superpanic.com>
2019-07-31 08:43:10 +0200

    Starfield working

--- Starfield/Source/main.lua
+++ Starfield/Source/main.lua
@@ -1,46 +1,72 @@
--- 5. do the same with C
-	
 import "CoreLibs/graphics"
+import "CoreLibs/utilities/fps"
 
 playdate.display.setScale(1)
-local screen_width = playdate.display.getWidth() + .0
-local screen_height = playdate.display.getHeight() + .0
+local screen_width = playdate.display.getWidth()
+local screen_height = playdate.display.getHeight()
 
-local nstars = 1000
+local nstars_list = {10,50,100,250,500,750,1000,1500}
+local nstars_list_len = 8
+local nstars_index = 1
+local nstars = nstars_list[nstars_index]
 
-local velocity = 1.2
-local star_x = {}
-local star_y = {}
+local speed = 0.0
+local stars = {}
 
-local star = {
-	xpos = 0,
-	ypos = 0,
-	velocity = 0
-}
+local font = playdate.graphics.loadFont("Font/Bitmore/font-Bitmore")
+playdate.graphics.setFont(font)
+
+function star() 
+	return {
+		xpos = (math.random(screen_width)  - screen_width / 2.0) / 100.0,
+		ypos = (math.random(screen_height) - screen_height / 2.0) / 100.0,
+		velocity = math.random(110,130) / 100.0
+	}
+end
 
 function playdate.update()
 	playdate.graphics.clear()
 	for i = 1, nstars do
-		local x = star_x[i] + screen_width / 2.0
-		local y = star_y[i] + screen_height / 2.0
-		playdate.graphics.fillCircleInRect(x, y, 5, 5)
-		star_x[i] *= velocity
-		star_y[i] *= velocity
-		
-		if x < 0 or x > screen_width then
-			star_x[i] = (math.random(screen_width) - screen_width / 2) / 100.0
+		local x = stars[i].xpos + screen_width  / 2.0
+		local y = stars[i].ypos + screen_height / 2.0
+		local size = (math.abs(stars[i].xpos) + math.abs(stars[i].ypos)) * 0.025 + 2.0
+		playdate.graphics.fillCircleInRect(x, y, size, size)
+		stars[i].xpos *= (stars[i].velocity + speed)
+		stars[i].ypos *= (stars[i].velocity + speed)
+		if x < 0 or x > screen_width or y < 0 or y > screen_height then
+			stars[i] = star()
 		end
-		if y < 0 or y > screen_height then
-			star_y[i] = (math.random(screen_height) - screen_height / 2.0) / 100.0
-		end
-		
+	end
+	fps(10,10)
+	playdate.graphics.drawText("*"..nstars_list[nstars_index].."*", 10,30 )
+end
+
+function playdate.cranked(change, acceleratedChange)
+	speed += change * 0.0001
+end
+
+function playdate.AButtonDown()
+	if playdate.graphics.getBackgroundColor() == playdate.graphics.kColorBlack then
+		playdate.graphics.setBackgroundColor(playdate.graphics.kColorWhite)
+	else
+		playdate.graphics.setBackgroundColor(playdate.graphics.kColorBlack)
 	end
 end
 
+function playdate.BButtonDown()
+	if nstars_index < nstars_list_len then
+		nstars_index++
+	else
+		nstars_index = 1
+	end
+	nstars = nstars_list[nstars_index]
+	setup()
+end
+
 function setup()
+	playdate.graphics.setColor(playdate.graphics.kColorXOR)
 	for i = 1, nstars do
-		star_x[i] = (math.random(screen_width) - screen_width / 2.0) / 100.0
-		star_y[i] = (math.random(screen_height) - screen_height / 2.0) / 100.0
+		stars[i] = star()
 	end
 end
 
