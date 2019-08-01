4a02149dac4592b98788daf7de95149a8736c845
Fredrik Josefsson <fredrik@superpanic.com>
2019-08-01 22:42:39 +0200

    working on animation and timers

--- GridGame/Source/Player/player.lua
+++ GridGame/Source/Player/player.lua
@@ -4,23 +4,53 @@ class('Player').extends(playdate.graphics.sprite)
 local playerImages = playdate.graphics.imagetable.new('Player/player')
 
 function Player:init()
-	Player.super.init(self)	
-	self:setImage(playerImages[1])
-	self.nImages = 2
-	self.currentImage = 1
+	Player.super.init(self)
 	self:add()
+	self.t = nil
+	self:setupFrames()
 end
 
 function Player:setupFrames()
-	self.kAnimationState = {IDLE, RIGHT}
+	self.kAnimationState = {
+		IDLE =  { 1, 2},
+		RIGHT = { 3, 4}
+	}
+	self.kAnimationSpeed = {
+		IDLE  = 1000 /  4,
+		RIGHT = 1000 /  4
+	}
+	self.currentState = self.kAnimationState.IDLE
+	self.animationSpeed = self.kAnimationSpeed.IDLE
+	self.frameIndex = 1
+	self:setImage(playerImages[self.currentState[self.frameIndex]])
+	self:nextImage()
 end
 
 function Player:nextImage()
-	if self.currentImage >= self.nImages then
-		self.currentImage = 1
+	if self.frameIndex >= #self.currentState then
+		self.frameIndex = 1
 	else
-		self.currentImage = self.currentImage + 1
+		self.frameIndex++
 	end
-	self:setImage(playerImages[self.currentImage])
+	self:setImage(playerImages[self.currentState[self.frameIndex]])
+	if self.t then self.t:remove() end
+	self.t = playdate.timer.new(self.animationSpeed, self.nextImage, self)
+end
+
+function Player:setAnimationIdle()
+	self.currentState = self.kAnimationState.IDLE
+	self.animationSpeed = self.kAnimationSpeed.IDLE
+	self.frameIndex=1
+	self:nextImage()
+end
+
+function Player:setAnimationRight()
+	self.currentState = self.kAnimationState.RIGHT
+	self.animationSpeed = self.kAnimationSpeed.RIGHT
+	self.frameIndex=1
+	self:nextImage()
+end
+
+function Player:setAnimation(state)
 end
 
--- GridGame/Source/main.lua
+++ GridGame/Source/main.lua
@@ -1,51 +1,61 @@
 import "CoreLibs/sprites"
 import "CoreLibs/graphics"
+import "CoreLibs/timer"
 import "Player/player"
 
 playdate.display.setScale(2)
-playdate.display.setRefreshRate(8)
+playdate.display.setRefreshRate(0)
 
 local gridSize = 16
 local gridOn = true
 local screenWidth = playdate.display.getWidth()
 local screenHeight = playdate.display.getHeight()
 
-local kGameState = {INITIAL, READY, PLAYING, PAUSED, OVER}
-local currentState = kGameState.INITIAL
+local kGameState = {
+	INITIAL = 1, 
+	READY   = 2, 
+	PLAYING = 3, 
+	PAUSED  = 4, 
+	OVER    = 5
+}
 
+local currentState = kGameState.INITIAL
 local player = Player()
 
 function playdate.update()
 	--playdate.graphics.clear()
-	player:nextImage()
+	--player:nextImage()
 	player:update()
-	draw_grid()
-end
-
-function draw_grid()
-	playdate.graphics.setDitherPattern(0.5)
+	-- draw grid
 	if gridOn then drawGrid() end
-	playdate.graphics.setDitherPattern(0.0)
+	-- drive timers
+	
+	playdate.timer.updateTimers()
 end
 
 function drawGrid()
+	playdate.graphics.setDitherPattern(0.5)
 	for x = gridSize, screenWidth, 16 do
 		playdate.graphics.drawLine( x, 0, x, screenHeight)
 	end
 	for y = gridSize, screenHeight, 16 do
 		playdate.graphics.drawLine(0, y, screenWidth, y)
 	end
+	playdate.graphics.setDitherPattern(0.0)
 end
 
 function setup()
 	-- setup stuff
-	player:moveTo(4*16+9,4*16+9)
+	player:moveTo(2*16+9,2*16+9)
 	player:update()
 end
 
-function playdate.AButtonDown()
-	-- invert color
-	--playdate.display.setInverted(not playdate.display.getInverted())
+function playdate.rightButtonDown()
+	player:setAnimationRight()
+end
+
+function playdate.rightButtonUp()
+	player:setAnimationIdle()
 end
 
 setup()
