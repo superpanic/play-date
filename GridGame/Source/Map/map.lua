c691872d5d745fa469beaced63dcbb2ee62c7563
Fredrik Josefsson <fredrik@superpanic.com>
2019-11-13 08:11:00 +0100

    new line of sight algo

--- GridGame/Source/Map/map.lua
+++ GridGame/Source/Map/map.lua
@@ -434,3 +434,105 @@ function Map:line_of_sight(p1, p2)
 end
 
 
+
+-- Bresenham-based Supercover line marching algorithm
+-- See: http://lifc.univ-fcomte.fr/home/~ededu/projects/bresenham/
+
+-- Note: This algorithm is based on Bresenham's line marching, but
+--  instead of considering one step per axis, it covers all the points
+--  the ideal line covers. It may be useful for example when you have 
+--  to know if an obstacle exists between two points (in which case the 
+--  points do not see each other)
+
+-- x1: the x-coordinate of the start point
+-- y1: the y-coordinate of the end point
+-- x2: the x-coordinate of the start point
+-- y2: the y-coordinate of the end point
+-- returns: an array of {x = x, y = y} pairs
+function Map:line_of_sight(x1, y1, x2, y2)
+	local points = {} -- create a list of points
+	local xstep, ystep, err, errprev, ddx, ddy -- some locals
+	local x, y = x1, y1 -- input start x and y
+	local dx, dy = x2 - x1, y2 - y1 -- calculate distance
+	
+	-- add the input start coordinates to the points list
+	points[#points + 1] = {x = x1, y = y1}
+
+	-- if destination is up (less than zero) invert step and distance
+	if dy < 0 then
+		ystep = ystep - 1
+		dy = -dy
+	else
+		ystep = 1
+	end
+
+	-- if destination is on the left side (less than zero, invert step and distance
+	if dx < 0 then
+		xstep = xstep - 1
+		dx = -dx
+	else
+		xstep = 1
+	end
+
+	-- ddx and ddy = double distance
+	ddx, ddy = dx * 2, dy * 2
+
+	-- 
+	if ddx >= ddy then
+		-- set previous error and current error
+		errprev, err = dx, dx
+		-- from 1 to x-distance
+		for i = 1, dx do
+			-- take one step
+			x = x + xstep
+			-- add Y doubledistance to error
+			err = err + ddy
+			-- if error has grown to more than double X distance
+			if err > ddx then
+				-- take one step in y direction
+				y = y + ystep
+				-- and reduce error with double X distance
+				err = err - ddx
+				
+				-- if current error + previous error is LESS than double x-distance 
+				if err + errprev < ddx then
+					-- add current point to points list
+					points[#points + 1] = {x = x, y = y - ystep}
+					
+				-- if current error + previous error is MORE than double x-distance
+				elseif err + errprev > ddx then
+					points[#points + 1] = {x = x - xstep, y = y}
+				
+				-- 
+				else
+					points[#points + 1] = {x = x, y = y - ystep}
+					points[#points + 1] = {x = x - xstep, y = y}
+				end
+			end
+			points[#points + 1] = {x = x, y = y}
+			errprev = err
+		end
+	else
+		errprev, err = dy, dy
+		for i = 1, dy do
+			y = y + ystep
+			err = err + ddx
+			if err > ddy then
+				x = x + xstep
+				err = err - ddy
+				if err + errprev < ddy then
+					points[#points + 1] = {x = x - xstep, y = y}
+				elseif err + errprev > ddy then
+					points[#points + 1] = {x = x, y = y - ystep}
+				else
+					points[#points + 1] = {x = x, y = y - ystep}
+					points[#points + 1] = {x = x - xstep, y = y}
+				end
+			end
+			points[#points + 1] = {x = x, y = y}
+			errprev = err
+		end
+	end
+	return points
+end
+	
